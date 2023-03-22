a20cccba884f087a3bffebbe90524d6f902ccfca
Fitrat <ozbekistond@gmail.com>
2023-03-22 10:49:13 +0500

    INIT: Projekt

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+Console.Write("Enter first number: ");
+int numberOne = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter second number: ");
+int numberTwo = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"\n{numberOne} + {numberTwo} = {numberOne+numberTwo}");
+Console.WriteLine($"{numberOne} - {numberTwo} = {numberOne-numberTwo}");
+Console.WriteLine($"{numberOne} : {numberTwo} = {numberOne/numberTwo} residual({numberOne%numberTwo})");
+Console.WriteLine($"{numberOne} * {numberTwo} = {numberOne*numberTwo}");
+
+Console.WriteLine("\nYou'll be healthy...");
